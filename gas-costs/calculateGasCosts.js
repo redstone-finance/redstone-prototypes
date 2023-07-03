360bea8774be6acea7194dd2d0b8f2a9e54d6fec
damiad <damian12msi@gmail.com>
2023-07-03 11:58:24 +0200

    refactor: added .env and contracts.json

--- gas-costs/calculateGasCosts.js
+++ gas-costs/calculateGasCosts.js
@@ -1,20 +1,32 @@
 const axios = require("axios");
+const dotenv = require("dotenv");
+const contracts = require("./contracts.json");
 
-// Replace '<YOUR__API_KEY>' with your actual Etherscan / Arbiscan API key
-const api_KEY = "<YOUR_API_KEY>";
+dotenv.config();
 
-const blockchainExplorer = "etherscan.io"; // or "arbiscan.io"
-const contractAddress = "0x68ba9602B2AeE30847412109D2eE89063bf08Ec2";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY;
 
-// todo: Remove 2 lines bellow after testing
-// const blockchainExplorer = "arbiscan.io"; // or "etherscan.io"
-// const contractAddress = "0x36497bcfea36a3ba831e8322cad35be1663d347c";
+const blockchainExplorerMap = {
+  ethereum: { explorer: "etherscan.io", apiKey: ETHERSCAN_API_KEY },
+  arbitrum: { explorer: "arbiscan.io", apiKey: ARBISCAN_API_KEY },
+};
 
-async function calculateCumulativeGasCosts(contractAddress) {
-  const apiUrl = `https://api.${blockchainExplorer}/api?module=account&action=txlist&address=${contractAddress}&apikey=${api_KEY}`;
+async function calculateCumulativeGasCosts(contractName) {
+  const contract = contracts.find((c) => c.name === contractName);
+  if (!contract) {
+    console.error("Contract not found in the config file");
+    return;
+  }
+
+  const { name, address, network } = contract;
+  const { explorer: blockchainExplorer, apiKey: API_KEY } =
+    blockchainExplorerMap[network];
+
+  const apiUrl = `https://api.${blockchainExplorer}/api?module=account&action=txlist&address=${address}&apikey=${API_KEY}`;
 
   try {
-    // Get the transaction history for the contract address from Etherscan API
+    // Get the transaction history for the contract address from the blockchain explorer API
     const response = await axios.get(apiUrl);
     const transactions = response.data.result;
 
@@ -28,12 +40,18 @@ async function calculateCumulativeGasCosts(contractAddress) {
       cumulativeGasCost += gasCost;
     }
 
-    cumulativeGasCost = cumulativeGasCost / 1e18; // gas costs are in gwei, so divide by 1e18 to convert to ETH
+    cumulativeGasCost = cumulativeGasCost / 1e18; // Gas costs are in gwei, so divide by 1e18 to convert to ETH
 
-    console.log("Cumulative Gas Costs:", cumulativeGasCost, "ETH");
+    console.log(
+      `Cumulative Gas Costs for contract '${name}':`,
+      cumulativeGasCost,
+      "ETH"
+    );
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-calculateCumulativeGasCosts(contractAddress);
+// Example usage
+calculateCumulativeGasCosts("swell");
+calculateCumulativeGasCosts("vesta");
