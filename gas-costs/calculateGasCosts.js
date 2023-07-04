6f97064178b8d8de0e4255262f76734df12610bb
damiad <damian12msi@gmail.com>
2023-07-04 14:06:41 +0200

    feat: transactions grouped by month, costs also in USD

--- gas-costs/calculateGasCosts.js
+++ gas-costs/calculateGasCosts.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 const dotenv = require("dotenv");
+const redstone = require("redstone-api");
 const contracts = require("./contracts.json");
 
-dotenv.config({ path: ".env.example" });
+dotenv.config();
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY;
@@ -12,7 +13,12 @@ const blockchainExplorerMap = {
   arbitrum: { explorer: "arbiscan.io", apiKey: ARBISCAN_API_KEY },
 };
 
-async function calculateCumulativeGasCosts(contractName) {
+function getNumberOfDaysInMonth(month) {
+  const [year, monthValue] = month.split("-");
+  return new Date(year, monthValue, 0).getDate();
+}
+
+async function cumulativeGasCost(contractName) {
   const contract = contracts.find((c) => c.name === contractName);
   if (!contract) {
     console.error("Contract not found in the config file");
@@ -30,23 +36,84 @@ async function calculateCumulativeGasCosts(contractName) {
     const response = await axios.get(apiUrl);
     const transactions = response.data.result;
 
-    let cumulativeGasCost = 0;
+    let cumulativeGasCostETH = 0;
+    let cumulativeGasCostUSD = 0;
+
+    // Group transactions by month
+    const transactionsByMonth = {};
+
+    // Prepare an array of promises for fetching ETH prices for each transaction
+    const ethPricePromises = transactions.map(async (tx) => {
+      const timeStamp = parseInt(tx.timeStamp) * 1000; // Convert seconds to milliseconds
+      const ethUSD = await redstone.getHistoricalPrice("ETH", {
+        date: timeStamp,
+      });
+      return ethUSD.value;
+    });
+
+    // Fetch ETH prices for all transactions in parallel
+    const ethPrices = await Promise.all(ethPricePromises);
 
     // Iterate through each transaction
-    for (const tx of transactions) {
+    for (let i = 0; i < transactions.length; i++) {
+      const tx = transactions[i];
       const gasUsed = parseInt(tx.gasUsed);
       const gasPrice = parseInt(tx.gasPrice);
-      const gasCost = gasUsed * gasPrice;
-      cumulativeGasCost += gasCost;
+      const gasCost = (gasUsed * gasPrice) / 1e18; // Gas costs are in gwei, so divide by 1e18 to convert to ETH
+
+      cumulativeGasCostETH += gasCost;
+      cumulativeGasCostUSD += gasCost * ethPrices[i];
+
+      const date = new Date(parseInt(tx.timeStamp) * 1000);
+      const month = `${date.getFullYear()}-${date.getMonth() + 1}`;
+
+      if (!transactionsByMonth[month]) {
+        transactionsByMonth[month] = {
+          transactions: [],
+          cumulativeGasCostETH: 0,
+          cumulativeGasCostUSD: 0,
+        };
+      }
+
+      transactionsByMonth[month].transactions.push(tx);
+      transactionsByMonth[month].cumulativeGasCostETH += gasCost;
+      transactionsByMonth[month].cumulativeGasCostUSD += gasCost * ethPrices[i];
     }
 
-    cumulativeGasCost = cumulativeGasCost / 1e18; // Gas costs are in gwei, so divide by 1e18 to convert to ETH
-
+    const numberOfTransactions = transactions.length;
+    console.log("Total number of transactions:", numberOfTransactions);
     console.log(
       `Cumulative Gas Costs for contract '${name}':`,
-      cumulativeGasCost,
-      "ETH"
+      cumulativeGasCostETH,
+      "ETH",
+      cumulativeGasCostUSD,
+      "USD"
     );
+    console.log("--------------------");
+
+    // Display grouped transactions by month
+    for (const month in transactionsByMonth) {
+      const { transactions, cumulativeGasCostETH, cumulativeGasCostUSD } =
+        transactionsByMonth[month];
+
+      const numberOfDays = getNumberOfDaysInMonth(month);
+      const averageTransactionsPerDay = transactions.length / numberOfDays;
+
+      console.log(`Month: ${month}`);
+      console.log("Number of transactions:", transactions.length);
+      console.log(
+        "Average number of transactions per day:",
+        +averageTransactionsPerDay.toFixed(2)
+      );
+      console.log(
+        "Cumulative Gas Costs:",
+        cumulativeGasCostETH,
+        "ETH",
+        cumulativeGasCostUSD,
+        "USD"
+      );
+      console.log("--------------------");
+    }
   } catch (error) {
     console.error("Error:", error);
   }
@@ -54,4 +121,4 @@ async function calculateCumulativeGasCosts(contractName) {
 
 // Example usage: node calculateGasCosts.js swell or npm start swell
 const contractName = process.argv[2];
-calculateCumulativeGasCosts(contractName);
+cumulativeGasCost(contractName);
