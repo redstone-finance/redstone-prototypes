a9c152e2787c19cb15b237e55e117746c9e14a91
damiad <damian12msi@gmail.com>
2023-08-30 12:38:43 +0200

    feat: balancerV2 implementation, not working correctly yet

--- liquidity-slippage/scripts/balancerV2-slip.js
+++ liquidity-slippage/scripts/balancerV2-slip.js
@@ -5,18 +5,22 @@ const path = require("path");
 const redstone = require("redstone-api");
 const constants = require("../utils/constants");
 const {
-  calculatePoolSize,
-  calcPriceSecondInFirst,
+  calculateAndWriteToCSV,
   getApproximateTokensAmountInPool,
-  calculatePriceDifference,
+  amountTradeXSlippage,
 } = require("../utils/common");
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
 
+// TODO: correct addresses, its usually multi swap so check which tokens
+// exactly do you swap or swap on multiple pools at once...
+
+// TODO: check if correct indexes on given pool...
 const addresses = [
   {
-    address:
+    address: "0x60d604890feaa0b5460b28a424407c24fe89374a",
+    poolId:
       "0x60d604890feaa0b5460b28a424407c24fe89374a0000000000000000000004fc",
     fee: 0.0, //todo: get fee
     cryptoASymbol: "swETH",
@@ -25,7 +29,8 @@ const addresses = [
     toIndex: 2,
   },
   {
-    address:
+    address: "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786",
+    poolId:
       "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786000000000000000000000593",
     fee: 0.0, //todo: get fee
     cryptoASymbol: "GHO",
@@ -34,7 +39,8 @@ const addresses = [
     toIndex: 2,
   },
   {
-    address:
+    address: "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786",
+    poolId:
       "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786000000000000000000000593",
     fee: 0.0, //todo: get fee
     cryptoASymbol: "GHO",
@@ -43,7 +49,8 @@ const addresses = [
     toIndex: 2,
   },
   {
-    address:
+    address: "0xcfae6e251369467f465f13836ac8135bd42f8a56",
+    poolId:
       "0xcfae6e251369467f465f13836ac8135bd42f8a56000000000000000000000591",
     fee: 0.0, //todo: get fee
     cryptoASymbol: "GHO",
@@ -53,8 +60,15 @@ const addresses = [
   },
 ];
 
-const { address, fee, cryptoASymbol, cryptoBSymbol, fromIndex, toIndex } =
-  addresses[2];
+const {
+  address,
+  poolId,
+  fee,
+  cryptoASymbol,
+  cryptoBSymbol,
+  fromIndex,
+  toIndex,
+} = addresses[2];
 
 const cryptoA = constants[cryptoASymbol];
 const cryptoB = constants[cryptoBSymbol];
@@ -63,65 +77,139 @@ const provider = new ethers.providers.JsonRpcProvider(
   `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`
 );
 
-const BPoolABI = [
-  //todo: get correct ABI or use different way to get pool data!
-  "function getSwapFeePercentage() external view returns (uint256)",
-  "function getRate() external view returns (uint256)",
-  "function getNormalizedWeights() external view returns (uint256[] memory)",
-  "function onSwap(uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out) external view returns (uint256)",
-  "function totalSupply() external view returns (uint256)",
-  "function getSpotPrice(uint256 tokenInIndex, uint256 tokenOutIndex) external view returns (uint256)",
-];
-const balancerPool = new ethers.Contract(address, BPoolABI, provider); //todo: not working?
+const BALANCER_VAULT_ADDRESS = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
+const SwapExactIn = 0;
+const AddressZero = "0x0000000000000000000000000000000000000000";
+const FUNDS = {
+  sender: AddressZero,
+  recipient: AddressZero,
+  fromInternalBalance: false,
+  toInternalBalance: false,
+};
 
-// const contract = balancerPool;
+const BPoolABI = [
+  "function getPool(bytes32) external view returns (uint8)",
+  {
+    inputs: [
+      {
+        internalType: "enum IVault.SwapKind",
+        name: "kind",
+        type: "uint8",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes32",
+            name: "poolId",
+            type: "bytes32",
+          },
+          {
+            internalType: "uint256",
+            name: "assetInIndex",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "assetOutIndex",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+          {
+            internalType: "bytes",
+            name: "userData",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct IVault.BatchSwapStep[]",
+        name: "swaps",
+        type: "tuple[]",
+      },
+      {
+        internalType: "contract IAsset[]",
+        name: "assets",
+        type: "address[]",
+      },
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "sender",
+            type: "address",
+          },
+          {
+            internalType: "bool",
+            name: "fromInternalBalance",
+            type: "bool",
+          },
+          {
+            internalType: "address payable",
+            name: "recipient",
+            type: "address",
+          },
+          {
+            internalType: "bool",
+            name: "toInternalBalance",
+            type: "bool",
+          },
+        ],
+        internalType: "struct IVault.FundManagement",
+        name: "funds",
+        type: "tuple",
+      },
+    ],
+    name: "queryBatchSwap",
+    outputs: [
+      {
+        internalType: "int256[]",
+        name: "",
+        type: "int256[]",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const contract = new ethers.Contract(
+  BALANCER_VAULT_ADDRESS,
+  BPoolABI,
+  provider
+);
 
 async function getPricesInEachOther(fromCrypto, toCrypto) {
-  // todo: rewrite to balancer V2
-  // const [balanceFrom, balanceTo] = await Promise.all([
-  //   contract.balances(fromIndex),
-  //   contract.balances(toIndex),
-  // ]);
-
-  // const poolSize = await calculatePoolSize(
-  //   ethers.utils.formatUnits(balanceFrom.toString(), fromCrypto.decimals),
-  //   ethers.utils.formatUnits(balanceTo.toString(), toCrypto.decimals),
-  //   fromCrypto,
-  //   toCrypto
-  // );
-
-  // const secondPriceInFirst = await contract.callStatic.get_dy(
-  //   fromIndex,
-  //   toIndex,
-  //   ethers.utils.parseUnits("1", fromCrypto.decimals)
-  // );
-
-  // const firstPriceInSecond = await contract.callStatic.get_dy(
-  //   toIndex,
-  //   fromIndex,
-  //   ethers.utils.parseUnits("1", toCrypto.decimals)
-  // );
-
-  // return [
-  //   poolSize,
-  //   ethers.utils.formatUnits(
-  //     firstPriceInSecond.toString(),
-  //     fromCrypto.decimals
-  //   ),
-  //   ethers.utils.formatUnits(
-  //     secondPriceInFirst.toString(),
-  //     toCrypto.decimals
-  //   ),
-  // ];
-
-  const [from, to] = [fromIndex, toIndex];
-  //todo: not working!
-  const priceInEachOther = await balancerPool.getSpotPrice(from, to);
-  return;
-  console.log(
-    `Price ${toCrypto.symbol} in ${fromCrypto.symbol}: ${priceInEachOther}`
+  const poolSize = await getApproximateTokensAmountInPool(
+    address,
+    fromCrypto,
+    toCrypto
   );
-  return priceInEachOther;
+
+  const secondPriceInFirst = await getOutAmount(
+    ethers.utils.parseUnits("1", fromCrypto.decimals),
+    fromCrypto,
+    toCrypto,
+    contract
+  );
+
+  const firstPriceInSecond = await getOutAmount(
+    ethers.utils.parseUnits("1", toCrypto.decimals),
+    toCrypto,
+    fromCrypto,
+    contract
+  );
+
+  // TODO: maybe parse Units, maybe not....
+  return [
+    poolSize,
+    ethers.utils.formatUnits(
+      firstPriceInSecond.toString(),
+      fromCrypto.decimals
+    ),
+    ethers.utils.formatUnits(secondPriceInFirst.toString(), toCrypto.decimals),
+  ];
 }
 
 async function getOutAmount(fromAmount, fromCrypto, toCrypto, contract) {
@@ -129,17 +217,37 @@ async function getOutAmount(fromAmount, fromCrypto, toCrypto, contract) {
   if (fromCrypto.symbol !== cryptoASymbol) {
     [from, to] = [toIndex, fromIndex];
   }
-  const outAmount = await contract.get_dy(
-    from,
-    to,
-    ethers.utils.parseUnits(fromAmount.toString(), fromCrypto.decimals)
-  ); //todo: change get_dy to balancer function maybe calcOutGivenIn
+
+  const batchSwapSteps = [
+    {
+      poolId: poolId,
+      assetInIndex: from,
+      assetOutIndex: to,
+      amount: fromAmount,
+      userData: "0x",
+    },
+  ];
+
+  const tokenAddresses = [fromCrypto.address, toCrypto.address];
+
+  const result = await contract.callStatic.queryBatchSwap(
+    SwapExactIn,
+    batchSwapSteps,
+    tokenAddresses,
+    FUNDS
+  );
+
+  console.log("Query Batch Swap Result:", result);
+  //TODO probably return results[1] or also parse it...
+
+  return 0;
+
   return ethers.utils.formatUnits(outAmount.toString(), toCrypto.decimals);
 }
 
 async function calculateSlippage(fromCrypto, toCrypto) {
-  // const [poolSize, firstPriceInSecond, secondPriceInFirst] =
-  await getPricesInEachOther(fromCrypto, toCrypto);
+  const [poolSize, firstPriceInSecond, secondPriceInFirst] =
+    await getPricesInEachOther(fromCrypto, toCrypto);
   return;
 
   // calculateAndWriteToCSV(
@@ -173,71 +281,3 @@ async function findSlippage() {
 findSlippage().catch((err) => {
   console.error("Error occurred:", err);
 });
-
-// async function prepareData(fromCrypto, toCrypto) {
-//   const normalizedWeights = await balancerPool.getNormalizedWeights();
-//   const normalizedWeightIn = normalizedWeights[0];
-//   const normalizedWeightOut = normalizedWeights[1];
-//   const rate = await balancerPool.getRate();
-//   const swapFeePercentage = await balancerPool.getSwapFeePercentage();
-//   const swapFee = swapFeePercentage / 1e18;
-//   const totalSupply = await balancerPool.totalSupply();
-//   console.log(`Swap fee: ${swapFeePercentage / 1e18}%`);
-//   console.log(`Rate: ${rate / 1e18}`);
-//   console.log(`Normalized weight in: ${normalizedWeightIn / 1e18}`);
-//   console.log(`Normalized weight out: ${normalizedWeightOut / 1e18}`);
-//   console.log(`Total supply: ${totalSupply / 1e18}`);
-
-//   feePercentage = await balancerPool.getSwapFeePercentage();
-//   console.log(`Swap fee: ${ethers.utils.formatUnits(feePercentage, 18)}%`);
-
-//   [swapFee, tokenBalanceIn, tokenWeightIn, tokenBalanceOut, tokenWeightOut] =
-//     await Promise.all([
-//       balancerPool.getSwapFee(),
-//       balancerPool.getBalance(fromCrypto.address),
-//       balancerPool.getDenormalizedWeight(fromCrypto.address),
-//       balancerPool.getBalance(toCrypto.address),
-//       balancerPool.getDenormalizedWeight(toCrypto.address),
-//     ]);
-//   secondPriceInFirst = calcPriceSecondInFirst(
-//     tokenBalanceIn,
-//     tokenBalanceOut,
-//     fromCrypto.decimals,
-//     toCrypto.decimals
-//   );
-
-//   await getApproximateTokensAmountInPool(poolAddress, fromCrypto, toCrypto);
-// }
-
-// async function getOutAmount(fromAmount, fromCrypto, toCrypto, contract) {
-//   // contract = balancerPool;
-//   const outAmount = await contract.calcOutGivenIn(
-//     tokenBalanceIn,
-//     tokenWeightIn,
-//     tokenBalanceOut,
-//     tokenWeightOut,
-//     ethers.utils.parseUnits(fromAmount.toString(), fromCrypto.decimals),
-//     swapFee
-//   );
-//   return ethers.utils.formatUnits(outAmount.toString(), toCrypto.decimals);
-// }
-
-// async function calculateSlippage(fromCrypto, toCrypto) {
-//   await prepareData(fromCrypto, toCrypto);
-//   console.log(
-//     `Price ${toCrypto.symbol} in ${fromCrypto.symbol}: ${secondPriceInFirst}`
-//   );
-//   const gasFee = ethers.utils.formatUnits(swapFee.toString(), 18);
-//   const firstPriceInUSD = await redstone.getPrice(fromCrypto.symbol);
-//   const results = await calculatePriceDifference(
-//     pricesUSD,
-//     firstPriceInUSD,
-//     secondPriceInFirst,
-//     gasFee,
-//     fromCrypto,
-//     toCrypto,
-//     getOutAmount,
-//     balancerPool
-//   );
-//   results.forEach((result) => console.log(result));
-// }
--- liquidity-slippage/utils/common.js
+++ liquidity-slippage/utils/common.js
@@ -144,7 +144,7 @@ async function callGetOutAmount(
   return differencePercentage;
 }
 
-function delay(ms = 1500) {
+function delay(ms = 2000) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
