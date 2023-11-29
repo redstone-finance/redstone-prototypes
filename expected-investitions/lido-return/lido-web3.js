7e3ec0a4fa9528e38d175095f269396e4915e07e
damiad <damian12msi@gmail.com>
2023-11-29 11:53:35 +0100

    feat: extensive weekly change lido

--- expected-investitions/lido-return/lido-web3.js
+++ expected-investitions/lido-return/lido-web3.js
@@ -47,26 +47,43 @@ async function getAverageBlockTime(provider) {
   }
 }
 
-async function readFunctionValueAtBlock(targetBlockNumber) {
-  try {
-    const block = await web3.eth.getBlock(targetBlockNumber);
-    if (block) {
-      const timestamp = Number(block.timestamp);
-      const date = new Date(timestamp * 1000);
-      const formattedDate = date.toLocaleString();
-      const result = await contract.methods
-        .stEthPerToken()
-        .call(null, targetBlockNumber);
-      //   console.log(
-      //     `Function value at block ${targetBlockNumber} (${formattedDate}):`,
-      //     fromWei(result, "ether").toString()
-      //   );
-      return fromWei(result, "ether").toString();
-    } else {
-      console.error("Error fetching block data.");
+async function readFunctionValueAtBlock(targetBlockNumber, retryCount = 5) {
+  for (let i = 0; i < retryCount; i++) {
+    try {
+      const block = await web3.eth.getBlock(targetBlockNumber);
+      if (block) {
+        const timestamp = Number(block.timestamp);
+        const date = new Date(timestamp * 1000);
+        const formattedDate = new Intl.DateTimeFormat("default", {
+          timeZone: "UTC",
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+        }).format(date);
+        // const formattedDate = date.toLocaleString();
+        const result = await contract.methods
+          .stEthPerToken()
+          .call(null, targetBlockNumber);
+        //   console.log(
+        //     `Function value at block ${targetBlockNumber} (${formattedDate}):`,
+        //     fromWei(result, "ether").toString()
+        //   );
+        return {
+          value: fromWei(result, "ether"),
+          date: formattedDate,
+          timestamp: timestamp,
+        };
+
+        // fromWei(result, "ether").toString();
+      } else {
+        console.error("Error fetching block data.");
+      }
+    } catch (error) {
+      console.error(`Error reading function value (Attempt ${i + 1}):`, error);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
     }
-  } catch (error) {
-    console.error("Error reading function value:", error);
   }
 }
 
@@ -74,20 +91,45 @@ async function readHistoricalValues() {
   const currentBlockNumber = Number(await web3.eth.getBlockNumber());
   const averageBlockTime = await getAverageBlockTime(web3.eth);
   const blocksToCheck = [];
-  for (let i = 0; i < 8; i++) {
+  const timesPerDay = 24;
+  const numOfDays = 9;
+  for (let i = 0; i <= numOfDays * timesPerDay; i++) {
     blocksToCheck.push(
       Math.floor(
-        Number(currentBlockNumber - (i * 24 * 60 * 60) / averageBlockTime)
+        Number(
+          currentBlockNumber -
+            (((i * 24) / timesPerDay) * 60 * 60) / averageBlockTime
+        )
       )
     );
   }
-//   console.log("blocksToCheck", blocksToCheck);
+  //   console.log("blocksToCheck", blocksToCheck);
   const blockValues = await Promise.all(
     blocksToCheck.map((blockNumber) => readFunctionValueAtBlock(blockNumber))
   );
-  console.log(blockValues[0]);
-  console.log(blockValues[7]);
-  calculateYearlyReturn(blockValues[7], blockValues[0]);
+  console.log(blockValues);
+  // make a set of different values and count their occurrences
+
+  const uniqueValues = [...new Set(blockValues.map((item) => item.value))];
+  console.log(uniqueValues);
+  const uniqueValuesWithCount = uniqueValues.map((value) => {
+    return {
+      value: value,
+      count: blockValues.filter((item) => item.value === value).length,
+    };
+  });
+  console.log(uniqueValuesWithCount);
+
+  console.log(uniqueValuesWithCount[0].value);
+  console.log(uniqueValuesWithCount[7].value);
+  calculateYearlyReturn(
+    uniqueValuesWithCount[7].value,
+    uniqueValuesWithCount[0].value
+  );
+
+  // console.log(blockValues[0]);
+  // console.log(blockValues[7]);
+  // calculateYearlyReturn(blockValues[7], blockValues[0]);
 }
 
 function calculateYearlyReturn(startWeekVal, EndWeekVal) {
