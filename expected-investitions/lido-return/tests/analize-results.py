7800493a0eedb7e429484d48f01a11d23388e1d2
damiad <damian12msi@gmail.com>
2023-12-05 16:21:10 +0100

    chore: analize infrequent updates

--- expected-investitions/lido-return/tests/analize-results.py
+++ expected-investitions/lido-return/tests/analize-results.py
@@ -2,15 +2,26 @@ import pandas as pd
 
 
 def find_decreasing_values(file_path):
+    shift = 2
     df = pd.read_csv(file_path)
     df["EndTime"] = pd.to_datetime(df["EndTime"])
 
+    prev_update = 20
     for i in range(len(df) - 1):
         current_row = df.iloc[i]
         next_row = df.iloc[i + 1]
 
-        if current_row["Value"] > next_row["Value"] and next_row["EndTime"].hour != 12:
-            print(f"Index: {i}, EndDate bellow: {next_row['EndTime']}")
+        if current_row["Value"] < next_row["Value"]:
+            print(f"ERROR Index: {i+shift}, EndDate above: {next_row['EndTime']}")
+
+        if current_row["Value"] > next_row["Value"]:
+            if prev_update < 20:
+                print(f"ERROR: prev_update < 20h Index: {i+shift}, EndDate above: {next_row['EndTime']}")
+            prev_update = 0
+            if next_row["EndTime"].hour != 12:
+                print(f"Not between 12:00-1:00 PM Index: {i+shift}, EndDate bellow: {next_row['EndTime']}")
+
+        prev_update += 1
 
 
 file_path = "lido-ratio-1h3Y.csv"
