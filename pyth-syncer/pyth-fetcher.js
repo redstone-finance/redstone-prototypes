91b63d4afcf83bf8ee5073b6c30fbefb128f4868
damiad <damian12msi@gmail.com>
2024-03-06 15:17:17 +0100

    feat: filtering repetitions

--- pyth-syncer/pyth-fetcher.js
+++ pyth-syncer/pyth-fetcher.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
-// const { ethers } = require('ethers');
-// const provider = new ethers.providers.JsonRpcProvider('URL_DO_RPC');
 const hermesApiBaseUrl = "https://hermes.pyth.network";
+const decimalsAccuracy = 2; // Adjusting has impact on filtering out repeated prices
 
 // https://pyth.network/developers/price-feed-ids
 const tokenIds = {
@@ -19,35 +18,111 @@ async function getPricesForTokens(tokenIdsArray, publishTime) {
     const response = await axios.get(url, {
       headers: { accept: "application/json" },
     });
-    return response.data.parsed;
+    return (
+      response.data.parsed.map((token) => {
+        return {
+          symbol: Object.keys(tokenIds).find(
+            (symbol) => tokenIds[symbol] === "0x" + token.id
+          ),
+          id: token.id,
+          price: Number(
+            token.price.price * Math.pow(10, token.price.expo)
+          ).toFixed(decimalsAccuracy),
+          timestamp: token.price.publish_time,
+        };
+      }) || []
+    );
   } catch (error) {
     console.error("Failed to fetch price data:", error.message);
     return null;
   }
 }
 
-async function getPricesForTokensFromBlockToBlock(startBlock, endBlock) {
-  const startTimestamp = 1709112229;
-  // (await provider.getBlock(startBlock)).timestamp;
-  // const endTimestamp = (await provider.getBlock(endBlock)).timestamp;
-
-  const tokenIdsArray = Object.values(tokenIds);
-  const priceData = await getPricesForTokens(tokenIdsArray, startTimestamp);
-
-  if (priceData) {
-    priceData.forEach((token) => {
-      const expoAdjustedPrice =
-        Number(token.price.price) * Math.pow(10, token.price.expo);
-      console.log(`ID: ${token.id}`);
-      console.log(`Price: ${expoAdjustedPrice.toFixed(2)}`);
-      console.log(`Publish Time: ${token.price.publish_time}`);
-      console.log("-----------------------------");
-    });
-  } else {
-    console.log("No price data available");
-  }
+function generateTimestamps(startTimestamp, endTimestamp) {
+  return Array.from(
+    { length: endTimestamp - startTimestamp + 1 },
+    (_, index) => startTimestamp + index
+  );
 }
 
-const startBlock = 123456; 
-const endBlock = 123556;
-getPricesForTokensFromBlockToBlock(startBlock, endBlock);
+function filterConsecutiveDuplicatePrices(priceData) {
+  Object.keys(priceData).forEach((token) => {
+    priceData[token] = priceData[token].filter(
+      (entry, index, array) =>
+        index === 0 ||
+        index === array.length - 1 ||
+        entry.price !== array[index - 1].price
+    );
+  });
+}
+
+function restructurePriceData(priceData) {
+  const structuredPriceData = {};
+  priceData.forEach((data) => {
+    if (data) {
+      data.forEach((token) => {
+        if (!structuredPriceData[token.symbol]) {
+          structuredPriceData[token.symbol] = [];
+        }
+        structuredPriceData[token.symbol].push({
+          price: token.price,
+          timestamp: token.timestamp,
+        });
+      });
+    }
+  });
+
+  filterConsecutiveDuplicatePrices(structuredPriceData);
+  return structuredPriceData;
+}
+
+async function getPricesForTokensFromTimestampToTimestamp(
+  startTimestamp,
+  endTimestamp
+) {
+  const timestamps = generateTimestamps(startTimestamp, endTimestamp);
+  const priceDataPromises = timestamps.map((timestamp) =>
+    getPricesForTokens(Object.values(tokenIds), timestamp)
+  );
+
+  const priceData = await Promise.all(priceDataPromises);
+
+  // if (priceData) {
+  //   console.log("Price data:", priceData);
+  // } else {
+  //   console.log("No price data available");
+  // }
+  // return priceData || [];
+  //for each token, build object with timestamp and price
+  //return array of objects
+
+  const structuredPriceData = restructurePriceData(priceData);
+  console.log("Structured price data:", structuredPriceData);
+  Object.keys(structuredPriceData).forEach((token) => {
+    console.log(
+      `Token: ${token}, length: ${structuredPriceData[token].length}`
+    );
+  });
+  return structuredPriceData;
+}
+
+async function getPricesForTokensFromTimestamp(startTimestamp) {
+  endTimestamp = Math.floor(Date.now() / 1000);
+  const priceData = getPricesForTokensFromTimestampToTimestamp(
+    startTimestamp,
+    endTimestamp
+  );
+}
+
+async function getPricesForTokensLast6mins() {
+  const endTimestamp = Math.floor(Date.now() / 1000);
+  const startTimestamp = endTimestamp - 6 * 60;
+  const priceData = getPricesForTokensFromTimestampToTimestamp(
+    startTimestamp,
+    endTimestamp
+  );
+}
+
+// const startTimestamp = 1709112229;
+// getPricesForTokensFromTimestamp(startTimestamp);
+getPricesForTokensLast6mins();
