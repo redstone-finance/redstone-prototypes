d2c384c037d24c882f2d8e76acb3cd1f67ed3906
damiad <damian12msi@gmail.com>
2024-04-11 14:57:22 +0200

    feat: tester for fetchers

---
+++ dex-tester/src/curve.js
@@ -0,0 +1,45 @@
+const ethers = require("ethers");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`
+);
+
+const abi = [
+  "function get_dy(int128 i, int128 j, uint256 dx) external view returns (uint256)",
+];
+const abiV2 = [
+  "function get_dy(uint256 i, uint256 j, uint256 dx) external view returns (uint256)",
+];
+
+async function getOutAmount(fromAmount, fromCrypto, toCrypto) {
+  const outAmount = await contract.callStatic.get_dy(
+    fromCrypto.index,
+    toCrypto.index,
+    ethers.utils.parseUnits(fromAmount.toString(), fromCrypto.decimals)
+  );
+  return ethers.utils.formatUnits(outAmount.toString(), toCrypto.decimals);
+}
+
+const pool = {
+  DEX: "curve-crv",
+  poolAddress: "0x4ebdf703948ddcea3b11f675b4d1fba9d2414a14",
+  tokenA: {
+    index: 0,
+    decimals: 18,
+    symbol: "crvUSD",
+  },
+  tokenB: {
+    index: 2,
+    decimals: 18,
+    symbol: "CRV",
+  },
+};
+
+const contract = new ethers.Contract(pool.poolAddress, abiV2, provider);
+
+const fromAmount = 1;
+// getOutAmount(fromAmount, pool.tokenA, pool.tokenB).then(console.log);
---
+++ dex-tester/src/uniV3.js
@@ -0,0 +1,124 @@
+const ethers = require("ethers");
+const dotenv = require("dotenv");
+const path = require("path");
+const Decimal = require("decimal.js");
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`
+);
+
+// const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"; // Uniswap V3 Quoter address
+const quoterAddress = "0x61fFE014bA17989E743c5F6cB21bF9697530B21e";
+// const abi = [
+//   "function quoteExactInputSingle(address tokenIn, address tokenOut, uint24 fee, uint256 amountIn, uint160 sqrtPriceLimitX96) external returns (uint256 amountOut)",
+// ];
+const abi = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "address", name: "tokenIn", type: "address" },
+          {
+            internalType: "address",
+            name: "tokenOut",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amountIn",
+            type: "uint256",
+          },
+          { internalType: "uint24", name: "fee", type: "uint24" },
+          {
+            internalType: "uint160",
+            name: "sqrtPriceLimitX96",
+            type: "uint160",
+          },
+        ],
+        internalType: "struct IQuoterV2.QuoteExactInputSingleParams",
+        name: "params",
+        type: "tuple",
+      },
+    ],
+    name: "quoteExactInputSingle",
+    outputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      {
+        internalType: "uint160",
+        name: "sqrtPriceX96After",
+        type: "uint160",
+      },
+      {
+        internalType: "uint32",
+        name: "initializedTicksCrossed",
+        type: "uint32",
+      },
+      { internalType: "uint256", name: "gasEstimate", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const contract = new ethers.Contract(quoterAddress, abi, provider);
+
+async function getOutAmount(fromAmount, fromCrypto, toCrypto) {
+  const amountOut = await contract.callStatic.quoteExactInputSingle({
+    tokenIn: fromCrypto.address,
+    tokenOut: toCrypto.address,
+    fee: pool.fee,
+    amountIn: ethers.utils.parseUnits(
+      fromAmount.toString(),
+      fromCrypto.decimals
+    ),
+    sqrtPriceLimitX96: 0,
+  });
+
+//   return new Decimal(amountOut[0].toString())
+//     .div(10 ** toCrypto.decimals)
+//     .toString();
+
+  return ethers.utils.formatUnits(amountOut[0], toCrypto.decimals);
+
+  //   const amountOut = await contract.callStatic.quoteExactInputSingle(
+  //     fromCrypto.address,
+  //     toCrypto.address,
+  //     pool.fee,
+  //     ethers.utils.parseUnits(fromAmount.toString(), fromCrypto.decimals),
+  //     0
+  //   );
+  //   return ethers.utils.formatUnits(amountOut.toString(), toCrypto.decimals);
+}
+
+const fromCrypto = {
+  symbol: "WETH",
+  address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  decimals: 18,
+};
+
+const toCrypto = {
+  symbol: "WBTC",
+  address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+  decimals: 8,
+};
+
+const pool = {
+  fee: 500,
+};
+
+const amountsIn = [0.0002, 0.002, 0.02, 0.2, 2, 20000];
+
+async function slippageTester() {
+  for (const amountIn of amountsIn) {
+    const amountOut = await getOutAmount(amountIn, fromCrypto, toCrypto);
+    console.log(
+      `Amount in: ${amountIn} ${fromCrypto.symbol}, amount out: ${amountOut} ${toCrypto.symbol}`
+    );
+  }
+}
+
+slippageTester().catch((err) => {
+  console.error("Error occurred:", err);
+});
